9a1c6659f9a75276e2a601a1cc6baa380c639cf9
Felix Schulze <felix@Admins-iMac.local>
2018-09-06 14:32:21 +0200

    removed apps

--- aliases.zsh
+++ aliases.zsh
@@ -3,7 +3,7 @@ alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
+weather() { curl -4 wttr.in/${1:-berlin} }
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
