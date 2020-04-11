77a2ba05025e16f66dc5503acfb11273939b2333
Felix Schulze <8378750+schulzefelix@users.noreply.github.com>
2020-04-11 14:35:40 +0200

    modify aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,8 @@
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-alias reloadcli="source $HOME/.zshrc"
+alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-berlin} }
+alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
@@ -19,10 +18,16 @@ alias code="cd $HOME/code"
 
 # Laravel
 alias a="php artisan"
-alias ams="php artisan migrate:fresh --seed"
+alias fresh="php artisan migrate:fresh --seed"
+alias seed="php artisan db:seed"
 alias serve='php artisan serve'
 alias artisan="php artisan"
 alias phpunit="vendor/bin/phpunit"
+
+# PHP
+alias php73="/usr/local/Cellar/php@7.3/7.3.13/bin/php"
+alias php72="/usr/local/Cellar/php@7.2/7.2.26/bin/php"
+alias cfresh="rm -rf vendor/ composer.lock && composer i"
 
 # Homestead
 function homestead() {
@@ -42,6 +47,19 @@ alias vmr='vagrant reload --provision'
 alias commit="git add . && git commit -m"
 alias gcommit="git add . && git commit"
 alias gst="git status"
+alias gb="git branch"
 alias gc="git checkout"
 alias gd="git diff"
 alias gl="git log --oneline --decorate --color"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force"
+alias nuke="git clean -df && git reset --hard"
+alias pop="git stash pop"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+alias wip="commit wip"
